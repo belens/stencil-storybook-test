74a52a681c8c098e9bd9e4f45157f6d758fef806
Sander Belen <sander.belen@persgroep.net>
2023-09-05 11:57:08 +0200

    make mycomponent work

--- src/components/my-component/my-component.stories.ts
+++ src/components/my-component/my-component.stories.ts
@@ -1,34 +1,20 @@
-// export default {
-//   // this creates a ‘Components’ folder and a ‘MyComponent’ subfolder
-//   title: 'Components/MyComponent',
-//   // this is an example of how to add actions to the story
-//   parameters: {
-//     actions: {
-//       argTypesRegex: '^on.*',
-//       handles: ['click'], // you can add custom events to this array to trigger actions
-//     },
-//   },
-// };
-
-// const Template = args => `<my-component first="${args.first}" middle="${args.middle}" last="${args.last}"></my-component>`;
-
-// export const Example = Template.bind({});
-// Example.args = {
-//   first: 'Winnie',
-//   middle: 'The',
-//   last: 'Pooh',
-// };
-
-// stories/Button.stories.jsx
-import { MyComponent } from './my-component';
-
 export default {
-  component: MyComponent,
+  // this creates a ‘Components’ folder and a ‘MyComponent’ subfolder
+  title: 'Components/MyComponent',
+  // this is an example of how to add actions to the story
+  parameters: {
+    actions: {
+      argTypesRegex: '^on.*',
+      handles: ['click'], // you can add custom events to this array to trigger actions
+    },
+  },
 };
 
-export const Basic = {
-  args: {
-    text: 'Hello World',
-    color: 'purple',
-  },
+const Template = args => `<my-component first="${args.first}" middle="${args.middle}" last="${args.last}"></my-component>`;
+
+export const Example = Template.bind({});
+Example.args = {
+  first: 'Winnie',
+  middle: 'The',
+  last: 'Pooh',
 };
