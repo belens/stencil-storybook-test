f8f5ad2efbd61359394efa844ba5dafbc5a5d4ef
Sander Belen <sander.belen@persgroep.net>
2023-09-06 11:29:17 +0200

    shorten code

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,48 +1,15 @@
 import { h } from '@stencil/core';
-// import { ButtonValues } from './values';
-// import { IconValues } from '../icon/values';
 
 const names = ['s'];
 const labels = [''];
 
 export default {
   title: 'Atoms/button',
-  argTypes: {
-    text: {
-      control: { type: 'text' },
-    },
-    icon: {
-      options: names,
-      control: { type: 'select', labels },
-    },
-    iconOnly: {
-      control: { type: 'boolean' },
-    },
-    variant: {
-      control: { type: 'inline-radio' },
-      options: [],
-    },
-    size: {
-      control: { type: 'inline-radio' },
-      options: [],
-    },
-    title: {
-      control: { type: 'text' },
-    },
-    disabled: {
-      control: { type: 'boolean' },
-    },
-    onButtonClick: { action: 'onButtonClick' },
-  },
   args: {
     text: 'Button text',
-    iconOnly: false,
-    variant: 'asdf',
-    size: 'asdf',
-    disabled: false,
   },
 };
 
-export const Default = () => {
-  return <div>asdf</div>;
+export const Default = args => {
+  return <div>asdf {args.text}</div>;
 };
